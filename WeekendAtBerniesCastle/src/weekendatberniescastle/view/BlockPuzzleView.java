ab536538550ba034f54b24ce0350fa3dc2a5e144
GildartsClive <cmicmuir@gmail.com>
2015-11-18 09:33:11 -0700

    Merge origin/master
    Conflicts:
    	WeekendAtBerniesCastle/src/weekendatberniescastle/view/HelpMenuView.java

---
+++ WeekendAtBerniesCastle/src/weekendatberniescastle/view/BlockPuzzleView.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package weekendatberniescastle.view;
+import java.util.Scanner;
+import weekendatberniescastle.WeekendAtBerniesCastle;
+import weekendatberniescastle.control.PuzzleControl;
+/**
+ *
+ * @author MIc
+ */
+public class BlockPuzzleView {
+    
+    public void blockPuzzleView(){
+        
+        this.displayDescription();
+        
+        
+    }
+
+    private void displayDescription() {
+        
+        System.out.println("\n\nthis will be the description");
+        
+    }
+    
+}
